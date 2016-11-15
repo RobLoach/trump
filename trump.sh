64884ac01915e0bdce22db046fa721abb5ae612d
Jonathan Wagener <jonathan.wagener@gmail.com>
2016-11-15 12:42:29 +0200

    Updating the brain and getting the script to work

--- trump.sh
+++ trump.sh
@@ -1,1 +1,26 @@
 #! /bin/bash
+
+# Pretty much all of the code from AskUbuntu: http://askubuntu.com/a/525624/170545 
+
+FILE="$(pwd)/very_good_brain.txt"
+
+# get line count for $FILE (simulate 'wc -l')
+lc=0
+while read -r line; do
+ ((lc++))
+done < $FILE
+
+# get a random number between 1 and $lc
+rnd=$RANDOM
+let "rnd %= $lc"
+((rnd++))
+
+# traverse file and find line number $rnd
+i=0
+while read -r line; do
+ ((i++))
+ [ $i -eq $rnd ] && break
+done < $FILE
+
+# output random line
+printf '%s\n' "＼(｀0´)／ - '$line'"
